38980ea323d33587909f9bb7ca6cd983decaa3e7
Dyas Dendi Andika <52526901+dydean32@users.noreply.github.com>
2020-03-25 10:04:43 +0700

    Update Multiple Inheritance.cpp

--- Multiple Inheritance.cpp
+++ Multiple Inheritance.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class orang
 {
     private :
-    char nama[30];
+    char nama[100];
     int umur;
     public :
     void get()
     {
-        cin.ignore();
-		cout<<"Nama\t\t: ";
-        cin.getline(nama,30);
+        cin.get();
+        cout<<"Nama\t\t: ";
+        cin.getline(nama,100);
         cout<<"Umur\t\t: ";
         cin>>umur;
     }
@@ -24,35 +24,36 @@ class orang
 class pendidikan
 {
     private:
-    char SMA[50];
-    int UAN;
+    char SMA[100];
+    float UAN;
     public:
     void get()
     {
-    	cin.ignore();
+        cin.ignore();
         cout<<"Nama SMA\t: ";
-        cin.getline(SMA,30);
+        cin.getline(SMA,100);
         cout<<"Nilai UAN\t: ";
         cin>>UAN;
     }
     void display()
     {
-        cout<<"Nama SMA\t: "<<SMA<<endl;
-        cout<<"Nilai UAN\t: "<<UAN<<endl;
+        cout<<"SMA\t\t: "<<SMA<<endl;
+        cout<<"UAN\t\t: "<<UAN<<endl;
     }
 };
 class mahasiswa : public orang, public pendidikan
 {
     private :
-    string kampus;
-    int IP;
+    char kampus[100];
+    float IP;
     public:
     void get()
     {
-    	orang::get();
-    	pendidikan::get();
+        orang::get();
+        pendidikan::get();
+        cin.get();
         cout<<"Nama Kampus\t: ";
-        cin>>kampus;
+        cin.getline(kampus,100);
         cout<<"IPK\t\t: ";
         cin>>IP;
     }
@@ -69,10 +70,10 @@ class pegawai : public orang
     private :
     string jabatan;
     int NIP;
-    public:
+    public :
     void get()
     {
-    	orang::get();
+        orang::get();
         cout<<"NIP\t\t: ";
         cin>>NIP;
         cout<<"Jabatan\t\t: ";
@@ -80,34 +81,33 @@ class pegawai : public orang
     }
     void display()
     {
-    	orang::display();
+        orang::display();
         cout<<"NIP\t\t: "<<NIP<<endl;
         cout<<"Jabatan\t\t: "<<jabatan<<endl;
     }
 };
 int main()
 {
-    int no;
-    cout<<"===MASUKAN DATA==="<<endl;
+int no;
+    cout<<"====MASUKKAN DATA===="<<endl;
     cout<<"1. Mahasiswa"<<endl;
     cout<<"2. Pegawai"<<endl;
-    cout<<"Masukan no profesi anda: ";
+    cout<<"Masukkan no profesi anda: ";
     cin>>no;
     cout<<"Isi data berikut:\n";
-    if (no == 1)
-    {
-        mahasiswa M1;
-    	M1.get();
-        cout<<"\n\n++++TAMPILAN DATA++++"<<endl;
-        M1.display();
-    }
-    else if (no ==2)
-    {
-        pegawai P1;
-        P1.get();
-        cout<<"\n\n++++TAMPILAN DATA++++"<<endl;
-        P1.display();
-    }
-    return 0;    
+if (no == 1)
+{
+    mahasiswa M1;
+    M1.get();
+    cout<<"\n\n++++TAMPILAN DATA++++"<<endl;
+    M1.display();
 }
-
+else    
+{
+    pegawai P1;
+    P1.get();
+    cout<<"\n\n++++TAMPILAN DATA++++"<<endl;
+    P1.display();
+}
+    return 0;
+}
